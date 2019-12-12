1e7c0a8c7dae90a63914f137a2158a78841f76c7
Syzygianinfern0 <spsharan2000@gmail.com>
2019-12-12 11:01:47 +0530

    DQN Models and Memory

--- Rainbow/2. DQN/config.py
+++ Rainbow/2. DQN/config.py
@@ -1,8 +1,20 @@
-MAX_EPISODES = 1000
-EXPERIENCE_SIZE = 100000
-BATCH_SIZE = 64
-GAMMA = 1.0
-MAX_EPSILON = EPSILON = 1.0
-MIN_EPSILON = 1e-2
-DECAY_RATE = 0.005
-LEARNING_RATE = 1e-2
+import torch
+
+env_name = 'CartPole-v1'
+goal_score = 200
+
+max_episodes = 1000
+initial_exploration = 1000
+
+experience_size = 1000
+batch_size = 32
+
+gamma = 0.99
+max_epsilon = 1.0
+min_epsilon = 0.01
+epsilon_decay = 0.005
+
+log_interval = 10
+lr = 0.001
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
